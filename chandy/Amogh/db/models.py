4eef1e2a95a94c5ac0c9798a7f7776fe2260b202
Admin <charleeagni12@gmail.com>
2017-07-08 20:59:26 +0530

    merge

--- chandy/Amogh/db/models.py
+++ chandy/Amogh/db/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
 
 class CreateUser(models.Model):
@@ -10,9 +11,11 @@ class CreateUser(models.Model):
     password = models.CharField(max_length=24, unique=False, blank=False)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
-# class Points(models.Model):
-#     user = models.ForeignKey(CreateUser)
-#
+
+class Points(models.Model):
+    user = models.ForeignKey(CreateUser)
+    points = models.IntegerField()
