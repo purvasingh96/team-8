d74e96d2be79e41daaf10c8848ba7bbbf2f4a32b
Admin <charleeagni12@gmail.com>
2017-07-08 16:28:31 +0530

    trycatched improved

--- chandy/own_template/trycatch.py
+++ chandy/own_template/trycatch.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-r = requests.get('https://docs.google.com/forms/d/e/1FAIpQLSdwDrwgqZ1N6uFiMf1kxg3RcmhrSZ8mzQ8JPyYVlkL4xM8wyw/viewform')
+r = requests.get('http://www.incometaxindia.gov.in/Pages/default.aspx')
 
 r.text
 
 soup = BeautifulSoup(r.text, 'html.parser')
-form = soup.find('form')
+form = soup.find_all('form')
+type(form)
+
 inputs = form.find_all('input')
 for i in inputs:
     if i['type'] != 'hidden':
@@ -16,7 +18,7 @@ print [(element['name'], element['value']) for element in form.find_all('input')
 
 
 class ParseWebsite():
-    def __init__(self,url):
+    def __init__(self,url,google=True):
         self.html = requests.get(url)
         soup = BeautifulSoup(self.html.text,'html.parser')
         form = soup.find('form')
@@ -25,15 +27,22 @@ class ParseWebsite():
         for i in inputs:
             form_element = {}
             if i['type'] != 'hidden':
-                print i['aria-label']
-                form_element['name'] = i['aria-label']
+                if google:
+                    form_element['name'] = i['aria-label']
+                else:
+                    try:
+                        form_element['name'] = i['placeholder']
+                    except Exception as e:
+                        form_element['name'] = i['name']
                 form_element['type'] = i['type']
                 self.all_inputs.append(form_element)
     def make_form(self):
         string_generated = ""
         string_generated += '<form method="get" action="dynamically_decided">'
         for i in self.all_inputs:
-            string_generated += '<input name="ident' + i['name'] + '" type="' + i['type'] + '" placeholder="' + i['name'] + '">'
+            string_generated += i['name'] + '</br> <input name="ident' + i['name'] + '" type="' + i['type'] + '" placeholder="' + i['name'] + '">'
+            string_generated += '</br>'
+        string_generated += '<input type=submit value="next">'
         string_generated += "</form>"
         return string_generated
 
